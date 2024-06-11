af285d0bec7dbab34f7d592c2dd3e50046362d8e
Odoevskiy.PE <!1488PashA1488!>
2024-06-11 12:03:40 +0300

    lab_4 fix

--- src/itmo/java/basics/lab4/Main.java
+++ src/itmo/java/basics/lab4/Main.java
@@ -1,67 +1,65 @@
 package itmo.java.basics.lab4;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
     private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        exercise1();
-        exercise2();
-        System.out.println("Результат: " + exercise3());
-        System.out.println("Результат: " + exercise4());
-        System.out.println("Результат: " + exercise5(new int[]{1, 2, 3, 4, 5}));
-        System.out.println("Результат: " + exercise5(new int[]{1}));
-        System.out.println("Результат: " + exercise5(new int[]{3, 2, 3, 4, 5}));
-        System.out.println("Результат: " + exercise6(new int[]{1, 2, 3, 4, 5}));
-        System.out.println("Результат: " + exercise6(new int[]{0, 2, 0, 4, 5}));
-        exercise21(new int[]{1, 2, 3, 4, 5});
-        exercise21(new int[]{6, 2, 3, 4, 5});
-        exercise22();
-        exercise23(new int[]{6, 2, 3, 4, 5});
-        exercise24(new int[]{1, 2, 3, 6, 7, 1});
+        printOddNumbers();
+        printNumbersDivisibleByTreeAndFive();
+        System.out.println("Результат: " + isSumOfTwoNumbersEqualThird());
+        System.out.println("Результат: " + isThirdNumberLargest());
+        System.out.println("Результат: " + isNumberThreeInStartOrEndOfArray(new int[]{1, 2, 3, 4, 5}));
+        System.out.println("Результат: " + isNumberThreeInStartOrEndOfArray(new int[]{1}));
+        System.out.println("Результат: " + isNumberThreeInStartOrEndOfArray(new int[]{3, 2, 3, 4, 5}));
+        System.out.println("Результат: " + isArrayContainsNumberOneOrThree(new int[]{1, 2, 3, 4, 5}));
+        System.out.println("Результат: " + isArrayContainsNumberOneOrThree(new int[]{0, 2, 0, 4, 5}));
+        isArraySortAsc(new int[]{1, 2, 3, 4, 5});
+        isArraySortAsc(new int[]{6, 2, 3, 4, 5});
+        fillArray();
+        swapFirstAndLastEement(new int[]{6, 2, 3, 4, 5});
+        findFirstUniqueNumber(new int[]{1, 2, 3, 6, 7, 1});
     }
 
-    private static void exercise1() {
-        for (int i = 0; i <= 100; i++) {
+    private static void printOddNumbers() {
+        for (int i = 1; i <= 100; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
     }
 
-    private static void exercise2() {
-        ArrayList<Integer> divisibleByThree = new ArrayList<>();
-        ArrayList<Integer> divisibleByFive = new ArrayList<>();
-        ArrayList<Integer> divisibleByThreeAndFive = new ArrayList<>();
+    private static void printNumbersDivisibleByTreeAndFive() {
+        int divisibleByTreeIndex = 0;
+        int divisibleByFiveIndex = 0;
+        int divisibleByTreeAndFiveIndex = 0;
+
+        int[] divisibleByTree = new int[100 / 3];
+        int[] divisibleByFive = new int[100 / 5];
+        int[] divisibleByTreeAndFive = new int[100 / (3 * 5)];
 
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0) {
-                divisibleByThree.add(i);
+                divisibleByTree[divisibleByTreeIndex++] = i;
             }
 
             if (i % 5 == 0) {
-                divisibleByFive.add(i);
+                divisibleByFive[divisibleByFiveIndex++] = i;
             }
 
             if (i % 3 == 0 && i % 5 == 0) {
-                divisibleByThreeAndFive.add(i);
+                divisibleByTreeAndFive[divisibleByTreeAndFiveIndex++] = i;
             }
         }
 
-        System.out.printf("Делится на 3: %s\n", arrayListToString(divisibleByThree));
-        System.out.printf("Делится на 5: %s\n", arrayListToString(divisibleByFive));
-        System.out.printf("Делится на 3 и на 5: %s\n", arrayListToString(divisibleByThreeAndFive));
+        System.out.println("Делится на 3: " + Arrays.toString(divisibleByTree));
+        System.out.println("Делится на 5: " + Arrays.toString(divisibleByFive));
+        System.out.println("Делится на 3 и на 5: " + Arrays.toString(divisibleByTreeAndFive));
     }
 
-    private static String arrayListToString(ArrayList<Integer> list) {
-        return list.stream().map(String::valueOf).collect(Collectors.joining(","));
-    }
-
-    private static boolean exercise3() {
+    private static boolean isSumOfTwoNumbersEqualThird() {
 
         System.out.print("Введите первое число: ");
         int x = scanner.nextInt();
@@ -73,7 +71,7 @@ public class Main {
         return (x + y) == z;
     }
 
-    private static boolean exercise4() {
+    private static boolean isThirdNumberLargest() {
         System.out.print("Введите первое число: ");
         int x = scanner.nextInt();
         System.out.print("Введите второе число: ");
@@ -84,11 +82,11 @@ public class Main {
         return y > x && z > y;
     }
 
-    private static boolean exercise5(int[] array) {
+    private static boolean isNumberThreeInStartOrEndOfArray(int[] array) {
         return array.length >= 2 && (array[0] == 3 || array[array.length - 1] == 3);
     }
 
-    private static boolean exercise6(int[] array) {
+    private static boolean isArrayContainsNumberOneOrThree(int[] array) {
         for(int item: array) {
             if (item == 1 || item == 3) {
                 return true;
@@ -98,7 +96,7 @@ public class Main {
         return false;
     }
 
-    private static void exercise21(int[] array) {
+    private static void isArraySortAsc(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (i != 0 && array[i - 1] >= array[i]) {
                 System.out.println("Please, try again");
@@ -109,7 +107,7 @@ public class Main {
         System.out.println("OK");
     }
 
-    private static void exercise22() {
+    private static void fillArray() {
         System.out.print("Array length: ");
         int arrayLength = scanner.nextInt();
 
@@ -122,7 +120,7 @@ public class Main {
         System.out.println("Result: " + Arrays.toString(array));
     }
 
-    private static void exercise23(int[] array1) {
+    private static void swapFirstAndLastEement(int[] array1) {
         int[] array2 = array1.clone();
         array2[0] = array1[array1.length - 1];
         array2[array1.length - 1] = array1[0];
@@ -131,7 +129,7 @@ public class Main {
         System.out.println("Array 2: " + Arrays.toString(array2));
     }
 
-    private static void exercise24(int[] array) {
+    private static void findFirstUniqueNumber(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = i; j < array.length; j++) {
                 if (i != j && array[i] == array[j]) {
